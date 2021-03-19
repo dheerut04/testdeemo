6f1b231406a0949090995d1af25424b298f30999
dheerut04 <80894161+dheerut04@users.noreply.github.com>
2021-03-19 10:03:04 +0530

    Add files via upload

---
+++ testdemo.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter any two numbers to perform arthametic operations :");
+	scanf("%d%d",&a,&b);
+	printf("addition = %d\n",a+b);
+	printf("substraction = %d\n",a-b);	
+	printf("product = %d\n",a*b);
+	printf("division = %d",a/b);
+	return 0;
+}
